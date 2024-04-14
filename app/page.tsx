54e8d42378fa6d2eb0d3584f994af47a7693050b
Aleksander <aleksievisa@gmail.com>
2024-04-14 15:13:21 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <iframe
+        src="https://my.spline.design/hoverscrolleffect-a73790351d27484a731e0a887d97dcbb/"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+      ></iframe>
+    </main>
+  )
+}
